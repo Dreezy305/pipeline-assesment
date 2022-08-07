d9715eda998beb80db9a165764929f91e60c9a24
Dreezy <bankoleidris@gmail.com>
2022-08-07 22:16:12 +0000

    age calculator

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/howOld/:dob", (req, res) => {
+  const birthday = req.params.dob;
+  const getAge = Math.floor(
+    (new Date() - new Date(birthday).getTime()) / 3.15576e10
+  );
+  console.log(getAge);
+
+  if (getAge) {
+    return res.status(200).json({
+      success: true,
+      age: getAge,
+      message: "Age calculated successfully",
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      message: "There was an error, pls enter the correct format(YYYY-MM-DD)",
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
