7009d0a586acdc94996c28d41690750dfc49dcd0
Dreezy <bankoleidris@gmail.com>
2022-08-08 09:43:06 +0000

    adding limiter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
