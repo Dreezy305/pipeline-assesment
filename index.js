da5e49df73162b82d82db59e0e0a04df8f91dfeb
Dreezy <bankoleidris@gmail.com>
2022-09-15 22:28:19 +0000

    rate limiter

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ const customRedisRateLimiter = async (req, res, next) => {
   }
 };
 
-app.get("/howOld/:dob", customRedisRateLimiter, (req, res) => {
+app.get("/howOld/:dob", (req, res) => {
   const birthday = req.params.dob;
   const regex = /^\d{4}-\d{2}-\d{2}$/;
 
